c03d8530b5a7c289e0826c7717f8229bdc97c4bf
gy <guanyu18518912569@gmail.com>
2025-12-24 16:02:22 +0800

    添加忽略文件#2

--- sql/add_points_field.sql
+++
@@ -1,2 +0,0 @@
--- 为用户表添加积分字段
-ALTER TABLE ums_user ADD COLUMN points BIGINT DEFAULT 0 COMMENT '用户积分（每消费1元=1积分）';
--- sql/pms_category_add_icon.sql
+++
@@ -1,9 +0,0 @@
--- 为 pms_category 表添加 icon 字段
--- 用于存储分类图标/图片的URL
-
-ALTER TABLE pms_category 
-ADD COLUMN `icon` VARCHAR(500) DEFAULT NULL COMMENT '分类图标/图片URL' 
-AFTER `sort`;
-
--- 如果需要回滚，执行以下语句：
--- ALTER TABLE pms_category DROP COLUMN `icon`;
